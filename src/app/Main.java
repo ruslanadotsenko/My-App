edf4a93171f8f835dc67c76df90ef88d96915b39
Ruslana Kolomatska <ruslana.dotsenko@gmail.com>
2023-08-25 12:13:21 +0300

    Minor changes

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,6 @@ package app;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("This is shop-online app.");
+        System.out.println("Shop-online for customers.");
     }
 }
